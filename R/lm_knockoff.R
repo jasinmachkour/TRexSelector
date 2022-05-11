254d34fde0a78b7b0003390c5839aa53f2802e36
SimonTien13 <simon-tien@gmx.de>
2022-05-11 15:47:11 +0200

    Refactor Knockoffs->Dummies

--- R/lm_knockoff.R
+++ R/lm_knockoff.R
@@ -1,16 +1,16 @@
 #' Perform one random experiment
 #'
-#' Run one random experiment of the T-Knock filter, i.e., generates knockoffs, appends them to the predictor matrix, and runs the forward selection algorithm until it is terminated after T_stop knockoffs have been selected.
+#' Run one random experiment of the T-Knock filter, i.e., generates dummies, appends them to the predictor matrix, and runs the forward selection algorithm until it is terminated after T_stop dummies have been selected.
 #'
 #' @param X Real valued predictor matrix.
 #' @param y Response vector.
-#' @param T_stop Number of included knockoffs after which the random experiments (i.e., forward selection processes) are stopped.
-#' @param L_val Number of knockoffs.
+#' @param T_stop Number of included dummies after which the random experiments (i.e., forward selection processes) are stopped.
+#' @param num_dummies Number of dummies
 #' @param method 'tknock' for T-Knock filter and 'tknock+GVS' for T-Knock+GVS filter.
 #' @param type 'lar' for 'LARS' and 'lasso' for Lasso.
 #' @param corr_max Maximum allowed correlation between any two predictors from different clusters.
 #' @param lambda_2_lars lambda_2-value for LARS-based Elastic Net.
-#' @param earlyStop If TRUE, then the forward selection process is stopped after T_stop knockoffs have been included. Otherwise the entire solution path is computed.
+#' @param early_stop If TRUE, then the forward selection process is stopped after T_stop dummies have been included. Otherwise the entire solution path is computed.
 #' @param lars_state LARS object (i.e., object of the class tlarsCpp). Contains variables associated with previous stopping point (necessary to restart forward selection selection process exactly where it was previously terminated).
 #' @param verbose If TRUE progress in computations is shown.
 #' @param intercept If TRUE an intercept is included.
@@ -18,7 +18,7 @@
 #' @param cor.structure TRUE/FALSE.
 #' @param empirical TRUE/FALSE.
 #'
-#' @return LARS object (i.e., object of the class tlarsCpp).
+#' @return LARS object (i.e., object of the class tlars_cpp).
 #'
 #' @import tlars
 #'
@@ -38,12 +38,12 @@
 lm_knockoff = function(X,
                        y,
                        T_stop = 1,
-                       L_val = ncol(X),
+                       num_dummies = ncol(X),
                        method = 'tknock',
                        type = 'lar',
                        corr_max = 0.5,
                        lambda_2_lars = NULL,
-                       earlyStop = TRUE,
+                       early_stop = TRUE,
                        lars_state,
                        verbose = TRUE,
                        intercept = FALSE,
@@ -54,31 +54,31 @@ lm_knockoff = function(X,
 
   if (T_stop == 1 || missing(lars_state) || is.null(lars_state)) {
     if (method == 'tknock') {
-      X_Knock = add_knockoffs(
+      X_dummy = add_dummies(
         X,
-        L_val = L_val,
+        num_dummies = num_dummies,
         cor.structure = cor.structure,
         empirical = empirical
       )
     } else{
-      X_Knock = add_knockoffs_GVS(X,
-                                  L_val = L_val,
+      X_dummy = add_dummies_GVS(X,
+                                  num_dummies = num_dummies,
                                   corr_max = corr_max)
       # Data modification for Elastic Net
-      p_Knock = ncol(X_Knock)
-      X_Knock = (1 / sqrt(1 + lambda_2_lars)) * rbind(X_Knock, diag(rep(sqrt(
+      p_dummy = ncol(X_dummy)
+      X_dummy = (1 / sqrt(1 + lambda_2_lars)) * rbind(X_dummy, diag(rep(sqrt(
         lambda_2_lars
-      ), times = p_Knock)))
-      y = append(y, rep(0, times = p_Knock))
+      ), times = p_dummy)))
+      y = append(y, rep(0, times = p_dummy))
       # Scale data again
-      X_Knock = scale(X_Knock)
+      X_dummy = scale(X_dummy)
       y = y - mean(y)
     }
     # Create new LARS object
     lars_state = tlars::tlars_model(
-      X = X_Knock,
+      X = X_dummy,
       y = y,
-      num_dummies = L_val,
+      num_dummies = num_dummies,
       verbose = FALSE,
       info = FALSE,
       intercept = intercept,
@@ -90,7 +90,7 @@ lm_knockoff = function(X,
   # Execute LARS step
   tlars::tlars(model = lars_state,
                T_stop = T_stop,
-               early_stop = earlyStop,
+               early_stop = early_stop,
                info = FALSE)
 
   return(lars_state)
