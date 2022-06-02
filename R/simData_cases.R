d600956fe19e2e87fd3446869257384be4cc5d85
jasinmachkour <jmachkour@web.de>
2022-06-02 15:02:41 +0200

    Rename genomics data files
    - simData_cases --> sim_data_cases
    - simData_controls --> sim_data_controls

--- R/simData_cases.R
+++
@@ -1,9 +0,0 @@
-#' Genotypes for cases
-#'
-#' A matrix containing the SNPs of the cases.
-#'
-#' @format An object of the class "SnpMatrix".
-#'
-#' @import snpStats
-#'
-"simData_cases"
--- R/simData_controls.R
+++
@@ -1,9 +0,0 @@
-#' Genotypes for controls.
-#'
-#' A matrix containing the SNPs of the controls.
-#'
-#' @format An object of the class "SnpMatrix".
-#'
-#' @import snpStats
-#'
-"sim_data_controls"
