6174762b02c7b1103e72c3fca63505b731d4541e
jasinmachkour <jmachkour@web.de>
2022-06-02 14:58:48 +0200

    Update function TPP()
    - Polish code and documentation
    - Add error control

--- R/TPP.R
+++ R/TPP.R
@@ -1,20 +1,70 @@
 #' True positive proportion (TPP)
 #'
-#' @param beta_hat Estimated support vector.
-#' @param beta True support vector.
+#' Computes the TPP based on the estimated and the true regression coefficient vectors.
+#'
+#' @param beta_hat Estimated regression coefficient vector.
+#' @param beta True regression coefficient vector.
 #' @param eps Numerical zero.
 #'
-#' @return TPP.
+#' @return True positive proportion (TPP).
 #'
 #' @export
-TPP <- function(beta_hat, beta, eps = .Machine$double.eps) {
-  if (sum(is.na(beta_hat)) == length(beta_hat)) {
-    tpp <- NA
-  } else if (sum(abs(beta) > eps) == 0) {
+#'
+#' @examples
+#' data("Gauss_data")
+#' X <- Gauss_data$X
+#' y <- c(Gauss_data$y)
+#' beta <- Gauss_data$beta
+#'
+#' set.seed(1234)
+#' res <- tknock(X, y)
+#' beta_hat <- res$beta.selected
+#'
+#' TPP(beta_hat = beta_hat, beta = beta)
+TPP <- function(beta_hat,
+                beta,
+                eps = .Machine$double.eps) {
+  # Remove all dimension attributes of length one
+  beta_hat <- drop(beta_hat)
+  beta <- drop(beta)
+
+  # Error control
+  if (!is.vector(beta_hat)) {
+    stop("'beta_hat' must be a vector.")
+  }
+
+  if (!is.numeric(beta_hat)) {
+    stop("'beta_hat' only allows numerical values.")
+  }
+
+  if (anyNA(beta_hat)) {
+    stop("'beta_hat' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (!is.vector(drop(beta))) {
+    stop("'beta' must be a vector.")
+  }
+
+  if (!is.numeric(beta)) {
+    stop("'beta' only allows numerical values.")
+  }
+
+  if (anyNA(beta)) {
+    stop("'beta' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (length(beta_hat) != length(beta)) {
+    stop("Length of beta_hat does not match length of beta.")
+  }
+
+  # Compute TPP
+  num_actives <- sum(abs(beta) > eps)
+  num_true_positives <- sum(abs(beta) > eps & abs(beta_hat) > eps)
+
+  if (num_actives == 0) {
     tpp <- 0
   } else {
-    tpp <- 100 * (sum(abs(beta) > eps &
-      abs(beta_hat) > eps) / sum(abs(beta) > eps))
+    tpp <- (num_true_positives / num_actives)
   }
   return(tpp)
 }
