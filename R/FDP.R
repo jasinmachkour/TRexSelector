cc0b16a1d164cf9f85672987400f12d961c76ad7
jasinmachkour <jmachkour@web.de>
2022-06-02 14:58:19 +0200

    Update function FDP()
    - Polish code and documentation
    - Add error control

--- R/FDP.R
+++ R/FDP.R
@@ -1,20 +1,71 @@
 #' False discovery proportion (FDP)
 #'
-#' @param beta_hat Estimated support vector.
-#' @param beta True support vector.
+#' Computes the FDP based on the estimated and the true regression coefficient vectors.
+#'
+#' @param beta_hat Estimated regression coefficient vector.
+#' @param beta True regression coefficient vector.
 #' @param eps Numerical zero.
 #'
-#' @return FDP
+#' @return False discovery proportion (FDP).
 #'
 #' @export
-FDP <- function(beta_hat, beta, eps = .Machine$double.eps) {
-  if (sum(is.na(beta_hat)) == length(beta_hat)) {
-    fdp <- NA
-  } else if (sum(abs(beta_hat) > eps) == 0) {
+#'
+#' @examples
+#' data("Gauss_data")
+#' X <- Gauss_data$X
+#' y <- c(Gauss_data$y)
+#' beta <- Gauss_data$beta
+#'
+#' set.seed(1234)
+#' res <- tknock(X, y)
+#' beta_hat <- res$beta.selected
+#'
+#' FDP(beta_hat = beta_hat, beta = beta)
+FDP <- function(beta_hat,
+                beta,
+                eps = .Machine$double.eps) {
+  # Remove all dimension attributes of length one
+  beta_hat <- drop(beta_hat)
+  beta <- drop(beta)
+
+  # Error control
+  if (!is.vector(beta_hat)) {
+    stop("'beta_hat' must be a vector.")
+  }
+
+  if (!is.numeric(beta_hat)) {
+    stop("'beta_hat' only allows numerical values.")
+  }
+
+  if (anyNA(beta_hat)) {
+    stop("'beta_hat' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (!is.vector(drop(beta))) {
+    stop("'beta' must be a vector.")
+  }
+
+  if (!is.numeric(beta)) {
+    stop("'beta' only allows numerical values.")
+  }
+
+  if (anyNA(beta)) {
+    stop("'beta' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (length(beta_hat) != length(beta)) {
+    stop("Length of beta_hat does not match length of beta.")
+  }
+
+  # Compute FDP
+  num_selected_var <- sum(abs(beta_hat) > eps)
+  num_false_positives <- sum(abs(beta) < eps & abs(beta_hat) > eps)
+
+  if (num_selected_var == 0) {
     fdp <- 0
   } else {
-    fdp <- 100 * (sum(abs(beta) < eps &
-      abs(beta_hat) > eps) / sum(abs(beta_hat) > eps))
+    fdp <- num_false_positives / num_selected_var
   }
+
   return(fdp)
 }
