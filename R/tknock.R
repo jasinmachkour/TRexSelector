3c324de7d0ab6e3fb527b09bd391139b503e18f6
jasinmachkour <jmachkour@web.de>
2022-06-13 19:53:16 +0200

    Update tknock()
    - Add error control
    - Add unit tests
    - Polish code
    - Polish documentation

--- R/tknock.R
+++ R/tknock.R
@@ -1,28 +1,32 @@
-#' Runs the T-Knock filter with the extended calibration algorithm
+#' Run the T-Knock filter
 #'
-#' The T-Knock filter performs high-dimensional variable selection for a user-defined target false discovery rate (target FDR).
+#' Run the T-Knock filter. The T-Knock filter performs fast variable selection in high-dimensional settings while
+#' controlling the false discovery rate (FDR) at a user-defined target level.
 #'
 #' @param X Real valued predictor matrix.
 #' @param y Response vector.
 #' @param tFDR Target FDR level (between 0 and 1, i.e., 0% and 100%).
 #' @param K Number of random experiments.
-#' @param max_LL Integer factor determining the maximum number of dummies as a multiple of p (i.e., num_dummies = max_LL * p).
-#' @param max_T_stop If TRUE the maximum number of dummies that can be included before stopping is set to ceiling(n / 2).
-#' @param method 'tknock' for T-Knock filter and 'tknock+GVS' for T-Knock+GVS filter.
+#' @param max_num_dummies Integer factor determining the maximum number of dummies as a multiple of the number of original variables p
+#' (i.e., num_dummies = max_num_dummies * p).
+#' @param max_T_stop If TRUE the maximum number of dummies that can be included before stopping is set to ceiling(n / 2),
+#' where n is the number of data points/observations.
+#' @param method 'tknock' for the T-Knock filter and 'tknock+GVS' for the T-Knock+GVS filter.
 #' @param type 'lar' for 'LARS' and 'lasso' for Lasso.
 #' @param corr_max Maximum allowed correlation between any two predictors from different clusters.
 #' @param lambda_2_lars lambda_2-value for LARS-based Elastic Net.
-#' @param parallel_process If TRUE random experiments are executed in parallel.
-#' @param parallel_max_cores Maximum number of cores to be used for parallel processing (default: number of physical cores).
-#' @param seed Seed for random number generator (only if parallel_process = TRUE).
+#' @param parallel_process Logical. If TRUE random experiments are executed in parallel.
+#' @param parallel_max_cores Maximum number of cores to be used for parallel processing
+#' (default: minimum{Number of random experiments K, number of physical cores}).
+#' @param seed Seed for random number generator (ignored if parallel_process = FALSE).
 #' @param eps Numerical zero.
-#' @param verbose If TRUE progress in computations is shown.
+#' @param verbose Logical. If TRUE progress in computations is shown.
 #'
 #' @return A list containing the support vector, and the values of T and L as determined by the extended calibration algorithm.
 #'
-#' @import parallel
-#' @import doParallel
-#' @import foreach
+#' @importFrom parallel detectCores makeCluster stopCluster
+#' @importFrom doParallel registerDoParallel
+#' @importFrom foreach getDoParWorkers registerDoSEQ
 #'
 #' @export
 #'
@@ -31,13 +35,14 @@
 #' X <- Gauss_data$X
 #' y <- c(Gauss_data$y)
 #' set.seed(1234)
-#' res <- tknock(X, y)
-#' res$beta.selected
+#' res <- tknock(X = X, y = y)
+#' selected_var <- res$selected_var
+#' selected_var
 tknock <- function(X,
                    y,
                    tFDR = 0.2,
                    K = 20,
-                   max_LL = 10,
+                   max_num_dummies = 10,
                    max_T_stop = TRUE,
                    method = "tknock",
                    type = "lar",
@@ -48,6 +53,105 @@ tknock <- function(X,
                    seed = NULL,
                    eps = .Machine$double.eps,
                    verbose = TRUE) {
+  # Error control
+  method <- match.arg(method, c("tknock", "tknock+GVS"))
+
+  type <- match.arg(type, c("lar", "lasso"))
+
+  if (!is.matrix(X)) {
+    stop("'X' must be a matrix.")
+  }
+
+  if (!is.numeric(X)) {
+    stop("'X' only allows numerical values.")
+  }
+
+  if (anyNA(X)) {
+    stop("'X' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (!is.vector(drop(y))) {
+    stop("'y' must be a vector.")
+  }
+
+  if (!is.numeric(y)) {
+    stop("'y' only allows numerical values.")
+  }
+
+  if (anyNA(y)) {
+    stop("'y' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (nrow(X) != length(drop(y))) {
+    stop("Number of rows in X does not match length of y.")
+  }
+
+  if (length(tFDR) != 1 ||
+      tFDR < 0 ||
+      tFDR > 1) {
+    stop("'tFDR' must be a number between 0 and 1 (including 0 and 1).")
+  }
+
+  if (length(K) != 1 ||
+      K < 2 ||
+      K %% 1 != 0) {
+    stop("The number of random experiments 'K' must be an integer larger or equal to 2.")
+  }
+
+  if (length(max_num_dummies) != 1 ||
+      max_num_dummies < 1 ||
+      max_num_dummies %% 1 != 0) {
+    stop("'max_num_dummies' must be an integer larger or equal to 1.")
+  }
+
+  if (method == "tknock+GVS") {
+    if (length(corr_max) != 1 ||
+        corr_max < 0 ||
+        corr_max > 1) {
+      stop("'corr_max' must have a value between zero and one.")
+    }
+
+    if (!is.null(lambda_2_lars)) {
+      if (length(lambda_2_lars) != 1 ||
+          lambda_2_lars < eps) {
+        stop("'lambda_2_lars' must be a number larger than zero.")
+      }
+    }
+  }
+
+  if (parallel_process &&
+      (length(parallel_max_cores) != 1 ||
+       parallel_max_cores %% 1 != 0 ||
+       parallel_max_cores < 2)) {
+    stop(
+      "For parallel processing at least two workers have to be registered:
+         'parallel_max_cores' must be an integer larger or equal to 2."
+    )
+  }
+
+  if (parallel_process &&
+       parallel_max_cores > min(K, max(
+         1, parallel::detectCores(logical = FALSE)
+       ))) {
+    parallel_max_cores_modified <-
+      min(K, max(1, parallel::detectCores(logical = FALSE)))
+    message(
+      paste0(
+        "For computing ",
+        K,
+        " random experiments, it is not useful to register ",
+        parallel_max_cores,
+        " workers. Setting parallel_max_cores = ",
+        min(K, max(
+          1, parallel::detectCores(logical = FALSE)
+        )),
+        " (# physical cores) ...\n"
+      )
+    )
+    parallel_max_cores <-
+      min(K, max(1, parallel::detectCores(logical = FALSE)))
+  }
+
   # Scale X and center y
   X <- scale(X)
   y <- y - mean(y)
@@ -72,9 +176,9 @@ tknock <- function(X,
     on.exit(foreach::registerDoSEQ(), add = TRUE)
   }
 
-  while ((LL <= max_LL &&
-    FDP_hat[which(abs(V - 0.75) < eps)] > tFDR) ||
-    sum(!is.na(FDP_hat)) == 0) {
+  while ((LL <= max_num_dummies &&
+          FDP_hat[which(abs(V - 0.75) < eps)] > tFDR) ||
+         sum(!is.na(FDP_hat)) == 0) {
     num_dummies <- LL * p
     LL <- LL + 1
 
@@ -89,12 +193,10 @@ tknock <- function(X,
       type = type,
       corr_max = corr_max,
       lambda_2_lars = lambda_2_lars,
-      earlyStop = TRUE,
+      early_stop = TRUE,
       verbose = verbose,
       intercept = FALSE,
-      normalize = TRUE,
-      cor.structure = FALSE,
-      empirical = FALSE,
+      standardize = TRUE,
       parallel_process = parallel_process,
       parallel_max_cores = parallel_max_cores,
       seed = seed,
@@ -114,6 +216,7 @@ tknock <- function(X,
       eps = eps
     )
 
+    # FDP_hat
     FDP_hat <- fdp_hat(
       V = V,
       Phi = Phi,
@@ -122,8 +225,9 @@ tknock <- function(X,
       num_dummies = num_dummies
     )
 
+    # Print number of appended dummies by the extended calibration algorithm of the T-Knock filter
     if (verbose) {
-      cat(paste("\n L =", num_dummies, "\n"))
+      cat(paste("\n Appended dummies:", num_dummies, "\n"))
     }
   }
 
@@ -156,13 +260,11 @@ tknock <- function(X,
       type = type,
       corr_max = corr_max,
       lambda_2_lars = lambda_2_lars,
-      earlyStop = TRUE,
+      early_stop = TRUE,
       lars_state_list = rand_exp$lars_state_list,
       verbose = verbose,
       intercept = FALSE,
-      normalize = TRUE,
-      cor.structure = FALSE,
-      empirical = FALSE,
+      standardize = TRUE,
       parallel_process = parallel_process,
       parallel_max_cores = parallel_max_cores,
       seed = seed,
@@ -185,6 +287,7 @@ tknock <- function(X,
       eps = eps
     )
 
+    # FDP_hat
     FDP_hat <- fdp_hat(
       V = V,
       Phi = Phi,
@@ -194,8 +297,9 @@ tknock <- function(X,
     )
     FDP_hat_mat <- rbind(FDP_hat_mat, FDP_hat)
 
+    # Print the number of by the extended calibration algorithm of the T-Knock filter included dummies along the selection process before stopping
     if (verbose) {
-      cat(paste("\n T =", T_stop, "\n"))
+      cat(paste("\n Included dummies before stopping:", T_stop, "\n"))
     }
   }
 
@@ -208,12 +312,13 @@ tknock <- function(X,
     Phi_mat = Phi_mat,
     V = V
   )
-  beta.selected <- res_T_dummy$beta.selected
+  selected_var <- res_T_dummy$selected_var
   v_thresh <- res_T_dummy$v_thresh
   R_mat <- res_T_dummy$R_mat
 
+  # List of results
   res <- list(
-    beta.selected = beta.selected,
+    selected_var = selected_var,
     tFDR = tFDR,
     T_stop = T_stop,
     num_dummies = num_dummies,
--- tests/testthat/test-tknock.R
+++ tests/testthat/test-tknock.R
@@ -1,3 +1,279 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# 1
+test_that("error control for inputs method and type works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(tknock(X = X,
+                      y = y,
+                      method = "test"))
+
+  expect_error(tknock(X = X,
+                      y = y,
+                      type = "test"))
 })
+
+# 2
+test_that("error control for input X works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  X_w_NA <- X
+  X_w_NA[sample(prod(dim(X)), size = 100)] <- NA
+
+  # Tests
+  expect_error(tknock(X = drop(X[, 1]),
+                      y = y),
+               "'X' must be a matrix.",
+               fixed = TRUE)
+
+  expect_error(tknock(X = matrix(as.character(X), ncol = ncol(X)),
+                      y = y),
+               "'X' only allows numerical values.",
+               fixed = TRUE)
+
+  expect_error(tknock(X = matrix(as.factor(X), ncol = ncol(X)),
+                      y = y),
+               "'X' only allows numerical values.",
+               fixed = TRUE)
+
+  expect_error(
+    tknock(X = X_w_NA,
+           y = y),
+    "'X' contains NAs. Please remove or impute them before proceeding.",
+    fixed = TRUE
+  )
+})
+
+# 3
+test_that("error control for input y works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  y_w_NA <- y
+  y_w_NA[sample(length(y), size = 10)] <- NA
+
+  # Tests
+  expect_error(tknock(X = X,
+                      y = cbind(y, y)),
+               "'y' must be a vector.",
+               fixed = TRUE)
+
+  expect_error(tknock(X = X,
+                      y = as.character(y)),
+               "'y' only allows numerical values.",
+               fixed = TRUE)
+
+  expect_error(tknock(X = X,
+                      y = matrix(as.factor(y), ncol = 1)),
+               "'y' only allows numerical values.",
+               fixed = TRUE)
+
+  expect_error(
+    tknock(X = X,
+           y = y_w_NA),
+    "'y' contains NAs. Please remove or impute them before proceeding.",
+    fixed = TRUE
+  )
+
+  expect_error(tknock(X = rbind(X, X),
+                      y = y),
+               "Number of rows in X does not match length of y.",
+               fixed = TRUE)
+})
+
+# 4
+test_that("error control for input tFDR works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    tknock(X = X,
+           y = y,
+           tFDR = -0.1),
+    "'tFDR' must be a number between 0 and 1 (including 0 and 1).",
+    fixed = TRUE
+  )
+
+  expect_error(
+    tknock(X = X,
+           y = y,
+           tFDR = -0.1),
+    "'tFDR' must be a number between 0 and 1 (including 0 and 1).",
+    fixed = TRUE
+  )
+})
+
+# 5
+test_that("error control for input K works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    tknock(X = X,
+           y = y,
+           K = 1),
+    "The number of random experiments 'K' must be an integer larger or equal to 2.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    tknock(X = X,
+           y = y,
+           K = 20.3),
+    "The number of random experiments 'K' must be an integer larger or equal to 2.",
+    fixed = TRUE
+  )
+})
+
+# 6
+test_that("error control for input max_num_dummies works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    tknock(X = X,
+           y = y,
+           max_num_dummies = 0),
+    "'max_num_dummies' must be an integer larger or equal to 1.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    tknock(X = X,
+           y = y,
+           max_num_dummies = 2.3),
+    "'max_num_dummies' must be an integer larger or equal to 1.",
+    fixed = TRUE
+  )
+})
+
+# 7
+test_that("error control for input corr_max works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    tknock(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      corr_max = -0.1
+    ),
+    "'corr_max' must have a value between zero and one.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    tknock(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      corr_max = 1.1
+    ),
+    "'corr_max' must have a value between zero and one.",
+    fixed = TRUE
+  )
+})
+
+# 8
+test_that("error control for input lambda_2_lars works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    tknock(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      lambda_2_lars = c(1, 5, 100)
+    ),
+    "'lambda_2_lars' must be a number larger than zero.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    tknock(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      lambda_2_lars = -3
+    ),
+    "'lambda_2_lars' must be a number larger than zero.",
+    fixed = TRUE
+  )
+})
+
+# 9
+test_that("error control for input parallel_max_cores works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    tknock(
+      X = X,
+      y = y,
+      parallel_process = TRUE,
+      parallel_max_cores = 1
+    ),
+    "For parallel processing at least two workers have to be registered:
+         'parallel_max_cores' must be an integer larger or equal to 2.",
+    fixed = TRUE
+  )
+})
+
+# 10
+test_that("reasonable number of workers is registered for parallel processing",
+          {
+            # Setup and data generation
+            data("Gauss_data")
+            X <- Gauss_data$X
+            y <- drop(Gauss_data$y)
+            K <- 20
+            parallel_max_cores <- 21
+
+            # Tests
+            expect_message(
+              tknock(
+                X = X,
+                y = y,
+                K = K,
+                parallel_process = TRUE,
+                parallel_max_cores = parallel_max_cores
+              ),
+              paste0(
+                "For computing ",
+                K,
+                " random experiments, it is not useful to register ",
+                parallel_max_cores,
+                " workers. Setting parallel_max_cores = ",
+                min(K, max(
+                  1, parallel::detectCores(logical = FALSE)
+                )),
+                " (# physical cores) ...\n"
+              ),
+              fixed = TRUE
+            )
+          })
