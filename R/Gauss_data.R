8b1f47a2d1241d4eb7fe22d7895936f512b5c618
jasinmachkour <jmachkour@web.de>
2022-06-13 19:42:31 +0200

    Polish 'Gauss_data'
    - supp --> support

--- R/Gauss_data.R
+++ R/Gauss_data.R
@@ -4,12 +4,12 @@
 #' and p = 100 variables (predictors), and a sparse parameter vector beta
 #' with associated support vector.
 #'
-#' @format A list containing a matrix X and vectors y, beta, and supp:
+#' @format A list containing a matrix X and vectors y, beta, and support:
 #' \describe{
 #'   \item{X}{Predictor matrix, n = 50, p = 100.}
 #'   \item{y}{Response vector.}
 #'   \item{beta}{Parameter vector.}
-#'   \item{supp}{Support vector.}
+#'   \item{support}{Support vector.}
 #' }
 #'
 #' @importFrom stats rnorm
@@ -21,12 +21,12 @@
 #' p <- 100
 #' X <- matrix(stats::rnorm(n * p), nrow = n, ncol = p)
 #' beta <- c(rep(5, times = 3), rep(0, times = 97))
-#' supp <- beta > 0
+#' support <- beta > 0
 #' y <- X %*% beta + stats::rnorm(n)
 #' Gauss_data <- list(
 #'   X = X,
 #'   y = y,
 #'   beta = beta,
-#'   supp = supp
+#'   support = support
 #' )
 "Gauss_data"
