a528b44ac453cb65f9804a4194d45bfad71ce945
jasinmachkour <jmachkour@web.de>
2022-06-13 19:50:52 +0200

    Update lm_dummy()
    - Add error control
    - Add unit tests
    - Polish code
    - Polish documentation

--- R/lm_dummy.R
+++ R/lm_dummy.R
@@ -1,27 +1,30 @@
 #' Perform one random experiment
 #'
-#' Run one random experiment of the T-Knock filter, i.e., generates dummies, appends them to the predictor matrix, and runs the forward selection algorithm until it is terminated after T_stop dummies have been selected.
+#' Run one random experiment of the T-Knock filter, i.e., generates dummies, appends them to the predictor matrix, and runs
+#' the forward selection algorithm until it is terminated after T_stop dummies have been selected.
 #'
 #' @param X Real valued predictor matrix.
 #' @param y Response vector.
+#' @param model_tlars Object of the class tlars_cpp. It contains all state variables of the previous T-LARS step (necessary for warm-starts, i.e., restarting
+#' the forward selection process exactly where it was previously terminated).
 #' @param T_stop Number of included dummies after which the random experiments (i.e., forward selection processes) are stopped.
-#' @param num_dummies Number of dummies
-#' @param method 'tknock' for T-Knock filter and 'tknock+GVS' for T-Knock+GVS filter.
+#' @param num_dummies Number of dummies that are appended to the predictor matrix.
+#' @param method 'tknock' for the T-Knock filter and 'tknock+GVS' for the T-Knock+GVS filter.
 #' @param type 'lar' for 'LARS' and 'lasso' for Lasso.
 #' @param corr_max Maximum allowed correlation between any two predictors from different clusters.
 #' @param lambda_2_lars lambda_2-value for LARS-based Elastic Net.
-#' @param early_stop If TRUE, then the forward selection process is stopped after T_stop dummies have been included. Otherwise the entire solution path is computed.
-#' @param lars_state LARS object (i.e., object of the class tlarsCpp). Contains variables associated with previous stopping point (necessary to restart forward selection selection process exactly where it was previously terminated).
-#' @param verbose If TRUE progress in computations is shown.
-#' @param intercept If TRUE an intercept is included.
-#' @param normalize If TRUE the predictors are standardized and the response is centered.
-#' @param cor.structure TRUE/FALSE.
-#' @param empirical TRUE/FALSE.
+#' @param early_stop Logical. If TRUE, then the forward selection process is stopped after T_stop dummies have been included. Otherwise
+#' the entire solution path is computed.
+#' @param verbose Logical. If TRUE progress in computations is shown when performing T-LARS steps on the created model.
+#' @param intercept Logical. If TRUE an intercept is included.
+#' @param standardize Logical. If TRUE the predictors are standardized and the response is centered.
 #'
-#' @return LARS object (i.e., object of the class tlars_cpp).
+#' @return Object of the class tlars_cpp.
 #'
-#' @import tlars
+#' @importFrom tlars tlars_model tlars tlars_cpp
 #' @importFrom glmnet cv.glmnet
+#' @importFrom stats rnorm
+#' @importFrom methods is
 #'
 #' @export
 #'
@@ -33,11 +36,13 @@
 #' X <- matrix(stats::rnorm(n * p), nrow = n, ncol = p)
 #' beta <- c(rep(3, times = 3), rep(0, times = 97))
 #' y <- X %*% beta + rnorm(n)
-#' res <- lm_dummy(X, y, T_stop = 1, num_dummies = 5 * p)
-#' support <- abs(res$get_beta()[seq(p)]) > eps
+#' res <- lm_dummy(X = X, y = y, T_stop = 1, num_dummies = 5 * p)
+#' beta_hat <- res$get_beta()[seq(p)]
+#' support <- abs(beta_hat) > eps
 #' support
 lm_dummy <- function(X,
                      y,
+                     model_tlars,
                      T_stop = 1,
                      num_dummies = ncol(X),
                      method = "tknock",
@@ -45,24 +50,104 @@ lm_dummy <- function(X,
                      corr_max = 0.5,
                      lambda_2_lars = NULL,
                      early_stop = TRUE,
-                     lars_state,
                      verbose = TRUE,
                      intercept = FALSE,
-                     normalize = TRUE,
-                     cor.structure = FALSE,
-                     empirical = FALSE) {
+                     standardize = TRUE) {
+  # Numerical zero
+  eps <- .Machine$double.eps
+
+  # Error control
   method <- match.arg(method, c("tknock", "tknock+GVS"))
 
-  if (T_stop == 1 || missing(lars_state) || is.null(lars_state)) {
-    if (method == "tknock") {
-      X_dummy <- add_dummies(
-        X = X,
-        num_dummies = num_dummies,
-        cor.structure = cor.structure,
-        empirical = empirical
+  type <- match.arg(type, c("lar", "lasso"))
+
+  if (!is.matrix(X)) {
+    stop("'X' must be a matrix.")
+  }
+
+  if (!is.numeric(X)) {
+    stop("'X' only allows numerical values.")
+  }
+
+  if (anyNA(X)) {
+    stop("'X' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (!is.vector(drop(y))) {
+    stop("'y' must be a vector.")
+  }
+
+  if (!is.numeric(y)) {
+    stop("'y' only allows numerical values.")
+  }
+
+  if (anyNA(y)) {
+    stop("'y' contains NAs. Please remove or impute them before proceeding.")
+  }
+
+  if (nrow(X) != length(drop(y))) {
+    stop("Number of rows in X does not match length of y.")
+  }
+
+  if (!(missing(model_tlars) || is.null(model_tlars))) {
+    if (!methods::is(object = model_tlars, class2 = tlars::tlars_cpp)) {
+      stop("'model_tlars' must be an object of class tlars_cpp.")
+    }
+  }
+
+  if (method == "tknock") {
+    if (length(num_dummies) != 1 ||
+        num_dummies %% 1 != 0 ||
+        num_dummies < 1) {
+      stop("'num_dummies' must be an integer larger or equal to 1.")
+    }
+  }
+
+  if (method == "tknock+GVS") {
+    if (length(num_dummies) != 1 ||
+        num_dummies %% ncol(X) != 0 ||
+        num_dummies < 1) {
+      stop(
+        "'num_dummies' must be a positive integer multiple of the total number of original predictors in X."
       )
+    }
+  }
+
+  if (length(T_stop) != 1 ||
+      !(T_stop %in% seq(1, num_dummies))) {
+    stop(
+      paste0(
+        "Value of 'T_stop' not valid. 'T_stop' must be an integer from 1 to ",
+        num_dummies,
+        "."
+      )
+    )
+  }
+
+  if (method == "tknock+GVS") {
+    if (length(corr_max) != 1 ||
+        corr_max < 0 ||
+        corr_max > 1) {
+      stop("'corr_max' must have a value between zero and one.")
+    }
+
+    if (!is.null(lambda_2_lars)) {
+      if (length(lambda_2_lars) != 1 ||
+          lambda_2_lars < eps) {
+        stop("'lambda_2_lars' must be a number larger than zero.")
+      }
+    }
+  }
+
+  # Create T-LARS model if it is not supplied (from a previous T-LARS step)
+  if (T_stop == 1 ||
+      missing(model_tlars) ||
+      is.null(model_tlars)) {
+    if (method == "tknock") {
+      X_Dummy <- add_dummies(X = X,
+                             num_dummies = num_dummies)
     } else {
-      X_dummy <- add_dummies_GVS(X = X,
+      X_Dummy <- add_dummies_GVS(X = X,
                                  num_dummies = num_dummies,
                                  corr_max = corr_max)
 
@@ -75,10 +160,10 @@ lm_dummy <- function(X,
             x = X,
             y = y,
             intercept = intercept,
-            standardize = normalize,
+            standardize = standardize,
             alpha = alpha,
             type.measure = "mse",
-            family = 'gaussian',
+            family = "gaussian",
             nfolds = 10
           )
         lambda_2_glmnet <- cvfit$lambda.1se
@@ -86,38 +171,38 @@ lm_dummy <- function(X,
       }
 
       # Data modification for Elastic Net
-      p_dummy <- ncol(X_dummy)
-      X_dummy <-
-        (1 / sqrt(1 + lambda_2_lars)) * rbind(X_dummy, diag(rep(sqrt(
+      p_dummy <- ncol(X_Dummy)
+      X_Dummy <-
+        (1 / sqrt(1 + lambda_2_lars)) * rbind(X_Dummy, diag(rep(sqrt(
           lambda_2_lars
         ), times = p_dummy)))
       y <- append(y, rep(0, times = p_dummy))
 
       # Scale data again
-      X_dummy <- scale(X_dummy)
+      X_Dummy <- scale(X_Dummy)
       y <- y - mean(y)
     }
 
     # Create new LARS object
-    lars_state <- tlars::tlars_model(
-      X = X_dummy,
+    model_tlars <- tlars::tlars_model(
+      X = X_Dummy,
       y = y,
       num_dummies = num_dummies,
       verbose = FALSE,
       info = FALSE,
       intercept = intercept,
-      standardize = normalize,
+      standardize = standardize,
       type = type
     )
   }
 
   # Execute LARS step
   tlars::tlars(
-    model = lars_state,
+    model = model_tlars,
     T_stop = T_stop,
     early_stop = early_stop,
     info = FALSE
   )
 
-  return(lars_state)
+  return(model_tlars)
 }
--- tests/testthat/test-lm_dummy.R
+++ tests/testthat/test-lm_dummy.R
@@ -1,3 +1,359 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# 1
+test_that("error control for inputs method and type works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(lm_dummy(
+    X = X,
+    y = y,
+    method = "test"
+  ))
+
+  expect_error(lm_dummy(
+    X = X,
+    y = y,
+    type = "test"
+  ))
+})
+
+# 2
+test_that("error control for input X works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  X_w_NA <- X
+  X_w_NA[sample(prod(dim(X)), size = 100)] <- NA
+
+  # Tests
+  expect_error(lm_dummy(
+    X = drop(X[, 1]),
+    y = y
+  ),
+  "'X' must be a matrix.",
+  fixed = TRUE
+  )
+
+  expect_error(lm_dummy(
+    X = matrix(as.character(X), ncol = ncol(X)),
+    y = y
+  ),
+  "'X' only allows numerical values.",
+  fixed = TRUE
+  )
+
+  expect_error(lm_dummy(
+    X = matrix(as.factor(X), ncol = ncol(X)),
+    y = y
+  ),
+  "'X' only allows numerical values.",
+  fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X_w_NA,
+      y = y
+    ),
+    "'X' contains NAs. Please remove or impute them before proceeding.",
+    fixed = TRUE
+  )
+})
+
+# 3
+test_that("error control for input y works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  y_w_NA <- y
+  y_w_NA[sample(length(y), size = 10)] <- NA
+
+  # Tests
+  expect_error(lm_dummy(
+    X = X,
+    y = cbind(y, y)
+  ),
+  "'y' must be a vector.",
+  fixed = TRUE
+  )
+
+  expect_error(lm_dummy(
+    X = X,
+    y = as.character(y)
+  ),
+  "'y' only allows numerical values.",
+  fixed = TRUE
+  )
+
+  expect_error(lm_dummy(
+    X = X,
+    y = matrix(as.factor(y), ncol = 1)
+  ),
+  "'y' only allows numerical values.",
+  fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y_w_NA
+    ),
+    "'y' contains NAs. Please remove or impute them before proceeding.",
+    fixed = TRUE
+  )
+
+  expect_error(lm_dummy(
+    X = rbind(X, X),
+    y = y
+  ),
+  "Number of rows in X does not match length of y.",
+  fixed = TRUE
+  )
+})
+
+# 4
+test_that(
+  "T-LARS model is an object of class tlars_cpp and stays an object of the same class after T-LARS steps",
+  {
+    # Setup and data generation
+    data("Gauss_data")
+    X <- Gauss_data$X
+    y <- drop(Gauss_data$y)
+
+    # Run one random experiment with T_stop = 1
+    mod_tlars <- lm_dummy(
+      X = X,
+      y = y,
+      T_stop = 1
+    )
+
+    # Tests
+    expect_true(methods::is(object = mod_tlars, class2 = tlars::tlars_cpp))
+
+    # Continue random experiment at the previous T-LARS state (warm start) until T_stop = 3 dummies have been selected
+    mod_tlars <- lm_dummy(
+      X = X,
+      y = y,
+      model_tlars = mod_tlars,
+      T_stop = 3
+    )
+
+    # Tests
+    expect_true(methods::is(object = mod_tlars, class2 = tlars::tlars_cpp))
+  }
+)
+
+# 5
+test_that("error control for input num_dummies works when method = 'tknock'", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  p <- ncol(X)
+  num_dummies <- 2 * p
+
+  # Tests
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      num_dummies = num_dummies + 1e-4,
+      method = "tknock"
+    ),
+    "'num_dummies' must be an integer larger or equal to 1.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      num_dummies = 0,
+      method = "tknock"
+    ),
+    "'num_dummies' must be an integer larger or equal to 1.",
+    fixed = TRUE
+  )
+})
+
+# 6
+test_that("error control for input num_dummies works when method = 'tknock+GVS'", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  p <- ncol(X)
+  num_dummies <- 2 * p
+
+  # Tests
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      num_dummies = num_dummies + 1e-4,
+      method = "tknock+GVS"
+    ),
+    "'num_dummies' must be a positive integer multiple of the total number of original predictors in X.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      num_dummies = 1.5 * p,
+      method = "tknock+GVS"
+    ),
+    "'num_dummies' must be a positive integer multiple of the total number of original predictors in X.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      num_dummies = 0,
+      method = "tknock+GVS"
+    ),
+    "'num_dummies' must be a positive integer multiple of the total number of original predictors in X.",
+    fixed = TRUE
+  )
+})
+
+# 7
+test_that("the input value of 'T_stop' is valid", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+  p <- ncol(X)
+  num_dummies <- 2 * p
+
+  # Tests
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      T_stop = 0,
+      num_dummies = num_dummies
+    ),
+    paste0(
+      "Value of 'T_stop' not valid. 'T_stop' must be an integer from 1 to ",
+      num_dummies,
+      "."
+    ),
+    fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      T_stop = num_dummies + 1,
+      num_dummies = num_dummies
+    ),
+    paste0(
+      "Value of 'T_stop' not valid. 'T_stop' must be an integer from 1 to ",
+      num_dummies,
+      "."
+    ),
+    fixed = TRUE
+  )
+})
+
+# 8
+test_that("error control for input corr_max works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      corr_max = -0.1
+    ),
+    "'corr_max' must have a value between zero and one.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      corr_max = 1.1
+    ),
+    "'corr_max' must have a value between zero and one.",
+    fixed = TRUE
+  )
+})
+
+# 9
+test_that("error control for input lambda_2_lars works", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      lambda_2_lars = c(1, 5, 100)
+    ),
+    "'lambda_2_lars' must be a number larger than zero.",
+    fixed = TRUE
+  )
+
+  expect_error(
+    lm_dummy(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      lambda_2_lars = -3
+    ),
+    "'lambda_2_lars' must be a number larger than zero.",
+    fixed = TRUE
+  )
+})
+
+# 10
+test_that("the user is informed that the entire solution path is computed if early_stop = FALSE", {
+  # Setup and data generation
+  data("Gauss_data")
+  X <- Gauss_data$X
+  y <- drop(Gauss_data$y)
+
+  # Tests
+  expect_message(
+    lm_dummy(
+      X = X,
+      y = y,
+      method = "tknock",
+      early_stop = FALSE
+    ),
+    "'T_stop' is ignored. Computing the entire solution path...",
+    fixed = TRUE
+  )
+
+  expect_message(
+    lm_dummy(
+      X = X,
+      y = y,
+      method = "tknock+GVS",
+      early_stop = FALSE
+    ),
+    "'T_stop' is ignored. Computing the entire solution path...",
+    fixed = TRUE
+  )
 })
